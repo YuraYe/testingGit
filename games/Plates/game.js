01b2d59786cc0be57804ffc3b22bd22e1e923f79
YuraYe <46287764+YuraYe@users.noreply.github.com>
2019-08-11 21:11:22 +0300

    Updated file games/Plates/game.js

--- games/Plates/game.js
+++ games/Plates/game.js
@@ -1,38 +1,75 @@
-let tiles
-var losers = []
+let tiles  = []
+let losers = []
+
 let LOSE = false
 let WINN = 0
+
+// Контрольные значения
+let cols = 2
+let rows = 1
+let step = 0
+
+let counter = 1
+let color   = 1
 
 $(function () {
     OnReload()
 })
 
 
+function OnReload (col = 2, row = 1, lose = 0) {
+
+    deleteTable()
+    tiles = []
+    losers = []
+
+    $('body').removeClass('win-body') // Очистка фона :)
+
+    // Запуск функций и присваивание значений
+    Draw(col, row)
+    tiles = $('.luckgame-table .a-tile')
+    SetLosers(lose + 1, tiles)
+    step++
+
+    // Событие нажатия на любую из плиток
+    $(tiles).on('click', function () {
+        TileOnClick(this)
+    })
+}
+
 function TileOnClick (obj) {
     if (!LOSE) {
         // Проигрыш и перезагрузка страницы после задержки в 1000мс
         if (losers[+$(obj).text()] == 1) {
-            $(obj).addClass('loser');
-            LOSE = true;
+            $(obj).addClass('loser')
+            LOSE = true
 
             setTimeout(function () {
                 window.location.reload(false)
             }, 1000)
         }
         // Делает плиточку зелёненькой - победоносненькой ))))))
-        else if (WINN <= 1) {
-            WINN++;
-            $(obj).addClass('winner');
+        else if (WINN <= tiles.length / 2) {
+            WINN++
+            $(obj).addClass('winner')
 
-            if (WINN == 1) {
+            // Победил, если открыл половину ячеек (все зелёные)
+            if (WINN == tiles.length / 2) {
                 setTimeout(function () {
-                    alert('CONGRATULATIONS! YOU WIN!!!')
-                }, 400)
+                    $('body').addClass('win-body') // Стиль победы для фона
+                }, 250)
                 setTimeout(function () {
-                    LOSE = false;
-                    WINN = 0;
-                    losers = OnReload(2, 2, 3)
-                    console.log(tiles)
+                    // Обнуление ключевых переменных победы и порожения
+                    LOSE = false
+                    WINN = 0
+                    
+                    if(rows % 4 == 0) {
+                    cols += 1;
+                    }
+                    OnReload(rows += 1, cols, tiles.length / 2)
+
+                    // Считаем количество пройденых уровней
+                    //counter++;
                 }, 500)
 
             }
@@ -41,27 +78,11 @@ function TileOnClick (obj) {
 }
 
 
-function OnReload (col = 2, row = 1, lose = 1) {
-
-    deleteTable()
-    tiles = []
-
-    // Запуск функций и присваивание значений
-    Draw(col, row)
-    tiles = $('.luckgame-table .a-tile')
-    SetLosers(lose, tiles)
-    console.log(losers)
-
-    // Событие нажатия на любую из плиток
-    $(tiles).on('click', function () {
-        TileOnClick(this)
-    })
-}
-
 
 // Генерируем таблицу 
 function Draw (col = 2, row = 1) {
     let id = 0;
+    color = rand(1, 5)
 
     // тег-обертка таблицы
     $('.luckgame-wrapper').append('<table class="luckgame-table"></table>')
@@ -70,7 +91,7 @@ function Draw (col = 2, row = 1) {
         $('.luckgame-table').append('<tr></tr>')
 
         for (let c = 0; c < col; c++) { // колонки
-            $('.luckgame-table>tr').last().append(`<th><div class="a-tile">${id}</div></th>`)
+            $('.luckgame-table>tr').last().append(`<th><div class="a-tile a-tile${color}">${id}</div></th>`)
             id++
         }
     }
@@ -83,7 +104,7 @@ function SetLosers (count = 1, tiles) {
     }
 
     while (true) {
-        let po = rand(0, tiles.length)
+        let po = rand(0, tiles.length - 1)
 
         // Проверка на повторы. 1 === "проиграть"
         if (losers[po] != 1) {
@@ -94,6 +115,7 @@ function SetLosers (count = 1, tiles) {
         if (count <= 0) // Выход из цикла
             break
     }
+    console.log(losers)
 }
 
 // Генерирует рандомное целое число от min до max. Самый лучший вариант =)
